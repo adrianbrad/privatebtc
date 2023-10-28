cd2c1a95164a1c912566cfcc419612c244f81058
adrianbrad <me@adrianbrad.com>
2023-10-28 11:12:23 +0300

    fix(goreleaser): fix goreleaser script

--- docker/doc.go
+++ docker/doc.go
@@ -1,1 +1,4 @@
+// Package docker provides utility functions for checking the Docker environment.
+// It also provides two implementations of the NodeService interface defined in the
+// privatebtc package.
 package docker
